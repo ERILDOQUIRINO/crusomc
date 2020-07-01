093dc52f1f9a22f22addaa97836b3781254e36ed
Erildo Quirino <equirino8179@gmail.com>
2020-06-30 21:45:38 -0300

    Ajuste EndPoint /categoria

--- src/main/java/com/equirino/cursomc/services/CategoriaService.java
+++ src/main/java/com/equirino/cursomc/services/CategoriaService.java
@@ -2,11 +2,14 @@ package com.equirino.cursomc.services;
 
 import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.equirino.cursomc.dominio.Categoria;
 import com.equirino.cursomc.repositories.CategoriaRepository;
+
+
 
 @Service
 public class CategoriaService {
@@ -17,7 +20,8 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		
 		Optional<Categoria>obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
 	}
 
 }
